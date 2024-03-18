c3ef19b6d15580949bb5e62b8eed9505a95329b5
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-03-18 23:52:48 +0530

    Update index.js

--- components/CryptocurrencyItem/index.js
+++ components/CryptocurrencyItem/index.js
@@ -1,16 +1,20 @@
 // Write your JS code here
 import './index.css'
 
-const CryptocurrencyItem = (props) => {
-    const {cryptoDetails} = props
-    const {currencyLogo,currencyName,usdValue,euroValue} = cryptoDetails
-    return(
-        <li className="crypto-list">
-            <div className="crypto-type-container">
-                <img src={currencyLogo} alt={currencyName} className="crypto-logo" />
-                <h1 className="heading">{currencyName}</h1>
-            </div>
-        </li>
-    )
+const CryptocurrencyItem = props => {
+  const {cryptoDetails} = props
+  const {currencyLogo, currencyName, usdValue, euroValue} = cryptoDetails
+  return (
+    <li className="crypto-list">
+      <div className="crypto-type-container">
+        <img src={currencyLogo} alt={currencyName} className="crypto-logo" />
+        <p className="crypto-name">{currencyName}</p>
+      </div>
+      <div className="crypto-values-container">
+        <p className="crypto-name">{usdValue}</p>
+        <p className="crypto-name">{euroValue}</p>
+      </div>
+    </li>
+  )
 }
 export default CryptocurrencyItem
